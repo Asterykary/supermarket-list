6c2928794500fc0bb74985b08f43beaaf1902231
Karina Celis <k_ale@outlook.com>
2020-08-31 21:33:14 -0400

    Se agrega firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDPRbu2ZexnNECTwBZSFTttzXJTo9WG2lo",
+    authDomain: "listasupermercado-9600e.firebaseapp.com",
+    databaseURL: "https://listasupermercado-9600e.firebaseio.com",
+    projectId: "listasupermercado-9600e",
+    storageBucket: "listasupermercado-9600e.appspot.com",
+    messagingSenderId: "10049482250",
+    appId: "1:10049482250:web:50b30e7a7d16e21ee6f9f3"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDPRbu2ZexnNECTwBZSFTttzXJTo9WG2lo",
+    authDomain: "listasupermercado-9600e.firebaseapp.com",
+    databaseURL: "https://listasupermercado-9600e.firebaseio.com",
+    projectId: "listasupermercado-9600e",
+    storageBucket: "listasupermercado-9600e.appspot.com",
+    messagingSenderId: "10049482250",
+    appId: "1:10049482250:web:50b30e7a7d16e21ee6f9f3"
+  }
 };
 
 /*
